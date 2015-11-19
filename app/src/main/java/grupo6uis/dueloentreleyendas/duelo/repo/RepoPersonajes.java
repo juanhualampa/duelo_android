953a69f4733696b7553f35b257dddfc921fbcaac
Juan Hualampa <juan.hualampa@gmail.com>
2015-11-19 17:53:01 -0300

    initial commit

---
+++ app/src/main/java/grupo6uis/dueloentreleyendas/duelo/repo/RepoPersonajes.java
@@ -0,0 +1,7 @@
+package grupo6uis.dueloentreleyendas.duelo.repo;
+
+/**
+ * Created by Juan on 19-Nov-15.
+ */
+public class RepoPersonajes {
+}
---
+++ app/src/main/java/grupo6uis/dueloentreleyendas/dueloApp/PersonajeSeleccionadoActivity.java
@@ -0,0 +1,13 @@
+package grupo6uis.dueloentreleyendas.dueloApp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonajeSeleccionadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personaje_seleccionado);
+    }
+}
