7ad6a4f040e288fd8e27ea2c3b326f007c1e21c2
Juan Hualampa <juan.hualampa@gmail.com>
2015-11-24 18:36:58 -0300

    ahora si

--- app/src/main/java/grupo6uis/dueloentreleyendas/dueloApp/SeleccionarPersonajeActivity.java
+++ app/src/main/java/grupo6uis/dueloentreleyendas/dueloApp/SeleccionarPersonajeActivity.java
@@ -11,12 +11,28 @@ import android.view.MenuItem;
 
 import grupo6uis.dueloentreleyendas.R;
 
+/**
+ * An activity representing a list of Personajes. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link PersonajeSeleccionadoActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ * <p/>
+ * The activity makes heavy use of fragments. The list of items is a
+ * {@link PersonajeSeleccionadoFragment} and the item details
+ * (if present) is a {@link PersonajeSeleccionadoFragment}.
+ * <p/>
+ * This activity also implements the required
+ * {@link SeleccionarPersonajeFragment.Callbacks} interface
+ * to listen for item selections.
+ */
 public class SeleccionarPersonajeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_seleccionar_personaje);
+        setContentView(R.layout.activity_duelo_app_bar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
