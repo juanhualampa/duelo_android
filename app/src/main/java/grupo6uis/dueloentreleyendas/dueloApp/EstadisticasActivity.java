082b1aed01267f74e1654e758989d3d354231822
luciano.federico.olmedo <luciano.federico.olmedo@gmail.com>
2015-11-25 20:56:38 -0300

    Refac: Llegamos a 3er pantalla, y el Rest 2

--- app/src/main/java/grupo6uis/dueloentreleyendas/dueloApp/EstadisticasActivity.java
+++ app/src/main/java/grupo6uis/dueloentreleyendas/dueloApp/EstadisticasActivity.java
@@ -2,11 +2,8 @@ package grupo6uis.dueloentreleyendas.dueloApp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 import grupo6uis.dueloentreleyendas.R;
@@ -16,8 +13,8 @@ public class EstadisticasActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_estadisticas);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_estadisticas_app_bar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar3);
         setSupportActionBar(toolbar);
 
         Intent intent = getIntent();
