cd68ce8ced2f4c838965fe405ac22a5c8a605cc6
luciano.federico.olmedo <luciano.federico.olmedo@gmail.com>
2015-11-27 16:14:39 -0300

    Refac for Services, but still not working

--- app/src/main/java/grupo6uis/dueloentreleyendas/dueloApp/SeleccionarPersonajeFragment.java
+++ app/src/main/java/grupo6uis/dueloentreleyendas/dueloApp/SeleccionarPersonajeFragment.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.ListView;
 
 import java.util.List;
-
 import grupo6uis.dueloentreleyendas.R;
 import grupo6uis.dueloentreleyendas.duelo.adapter.PersonajeAdapter;
 import grupo6uis.dueloentreleyendas.duelo.domain.Personaje;
